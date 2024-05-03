7e485746048bc6e4ecee92c65bf1e9cae7064fd3
rclaassen023 <rclaassen023@student.wethinkcode.co.za>
2024-05-03 21:21:33 +0200

    added path to server.js so that it can reach static route of frontend and added action and method to form in index.html

--- number-guessing-game/backend/server.js
+++ number-guessing-game/backend/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path')
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname,'..','frontend')));
 
 function generateRandomNumber() {
     return Math.floor(Math.random() * 100) + 1;
